f1713d2a36124395c1b9dee521880d48a8e6961d
Alex Babis <acbabis@gmail.com>
2018-03-13 17:08:29 -0700

    Showing intervals as numbers in table

--- src/PresenceHistory/PresenceHistoryBody.js
+++ src/PresenceHistory/PresenceHistoryBody.js
@@ -3,10 +3,20 @@ import './PresenceHistory.css';
 
 class PresenceHistoryBody extends Component {
   render() {
-    const {intervals} = this.props.data;
+    const {intervals, activity} = this.props.data;
+    // Deltas may not be uniform (eg DST), so precompute them
+    const deltas = intervals.slice(0, -1)
+      .map((interval, index) => intervals[index + 1] - interval);
     return <tbody>
+      {
+        activity.map(({nick, activity}, index) => <tr className="user-activity" key={index}>
+          <td className="lead">{nick}</td>
+          {activity.map(({online, idle}, index) => <td className="time-slot" key={index}>{
+            (online / deltas[index]).toFixed(1)
+          }</td>)}
+        </tr>)
+      }
       <tr>
-        <td className="lead"></td>
         {intervals.map((time, index) => <td key={index}></td>)}
       </tr>
     </tbody>
